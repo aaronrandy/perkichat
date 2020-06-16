bd6387f7f937b96fbadddbc9ae0e5eed8bd55ba8
Max <maximilian.mosch@smail.th-koeln.de>
2020-06-16 20:13:38 +0200

    login.js weiterleitung nach erfolgreichem login zu loginstartseite.html

--- public/src/loginstartseite.js
+++ public/src/loginstartseite.js
@@ -12,21 +12,19 @@ function loadDoc() {
         }
     };
 
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value
-    var name = document.getElementById("lastname").value;
-    var vorname = document.getElementById("name").value;
-    var adresse = document.getElementById("adresse").value;
-    var email = document.getElementById("email").value;
-
-    var zahlungsart = document.querySelector('input[name ="zahlungsart"]:checked').value;
-
-
 
     xhttp.open("POST", "https://parkouni.tk/api/Register?apikey=101", true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send("username="+username+"&password="+password+"&name="+name+"&vorname="+vorname+"&adresse="+adresse+"&zahlungsart="+zahlungsart+"&email="+email);
 
+}
+
+
+function getCookie() {
+    var nameEquals ="logindaten=";
+    var whole_cookie=document.cookie.split(nameEquals)[1].split(";")[0];
+    console.log(whole_cookie);
+    return whole_cookie;
 }
 
 function readCookie(cname){
@@ -58,38 +56,5 @@ function checkCookie() {
 }
 
 
-function signup(){
-    var p1 = document.getElementById("password").value;
-    var p2 = document.getElementById("password1").value;
-    if (p1 != p2 ){
-        document.getElementById("info").innerHTML="Passwort stimmt nicht überein" ;
-        return false ;
-    }else{
-        loadDoc();
-    }
-}
-
-function formSubmit(event) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 ) {
-
-            var data = JSON.parse(xhttp.responseText);
-            console.log(data.Status);
-            if(data.Status == true)
-                document.getElementById("info").innerHTML = "Erfolgreich Registriert";
-            else {
-                document.getElementById("info").innerHTML =  data.Information;
-                return false ;
-            }
-        }
-    };
-
-    xhttp.open("POST", "http://parkouni.tk/api/Register?apikey=101", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    request.send(new FormData(event.target)); // create FormData from form that triggered event
-    event.preventDefault();
-}
 
 window.onbeforeunload = function() { return "Your work will be lost."; };
