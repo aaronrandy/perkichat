d1968a4d085674eb49083d7d70cdb3012f5d691b
AjBanana <arnejakob473@gmail.com>
2020-06-18 00:38:20 +0200

    Vorbestellen.html angefangen

---
+++ public/src/Vorbestellen/vorbestellen.js
@@ -0,0 +1,8 @@
+function showCalendar() {
+    var x = document.getElementById("calendarID");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
