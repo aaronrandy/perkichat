b3ef91e12e08f69973bbcf83a2d5d273bb2988f6
aomari99 <aomari99@users.noreply.github.com>
2020-06-18 03:05:04 +0200

    add vehicles to profile

--- public/src/profile.js
+++ public/src/profile.js
@@ -17,6 +17,7 @@ function loadDoc() {
                 document.getElementById('inf').innerHTML = "Information: "+ Kunde.Information;
                 document.getElementById('email').innerHTML = "Email: "+ Kunde.email;
                 document.getElementById('pay').innerHTML = "Payment Method: "+ Kunde.KontoTyp;
+                
             }
             else
                 window.location.replace("https://parkouni.tk/404");
@@ -31,6 +32,41 @@ function loadDoc() {
 }
 
 
+function getcars() {
+    var username = getCookie().split("&")[1];
+    var password = getCookie().split("&")[0];
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            var data = JSON.parse(xhttp.responseText);
+            console.log(data.Status);
+            if(data.Status == true ){
+                var Kunde = JSON.parse(data.Information);
+                var inf= "";
+                console.log(Kunde.Information);
+                Kunde.forEach(x => {
+                  inf +=  x.Kennzeichen;
+                });
+                document.getElementById('vec').innerHTML = "Vehicles: "+ inf;
+                
+            }
+            else
+                document.getElementById('vec').innerHTML = "Vehicles: No Vehicles";
+        }
+        else
+            window.location.replace("https://parkouni.tk/404"); 
+    };
+
+    console.log("Username: " +username + "Passwort: "+password);
+    xhttp.open("POST", "https://parkouni.tk/api/Fahrzeuge?apikey=101", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("username="+username+"&password="+password);
+   
+}
+
+
+
 function getCookie() {
     var nameEquals ="logindaten=";
     var whole_cookie=document.cookie.split(nameEquals)[1].split(";")[0];   
@@ -39,3 +75,4 @@ function getCookie() {
 }
 
 loadDoc();
+getcars();
