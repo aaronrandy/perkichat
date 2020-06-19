8c88616df84d498d6a89cbe46b1f89eee4820817
aomari99 <aomari99@users.noreply.github.com>
2020-06-19 17:26:25 +0200

    Update loginstartseite.js

--- public/src/loginstartseite.js
+++ public/src/loginstartseite.js
@@ -43,8 +43,8 @@ function loadVorbestellung() {
                     ' <td>'+x.bis+'</td> \n'+
                     ' <td>'+x.Kennzeichen+'</td> \n'+
                     ' <td>'+x.Bemerkung+'</td> \n'+
-                        '<td class="btn btn-primary"> Bearbeiten </td> \n'+
-                         '<td onclick="deleteVor('+x.von+", "+x.bis+", "+x.Kennzeichen+');" class="btn btn-danger">Stornieren</td> \n'+
+                    '<td > Bearbeiten </td> \n'+
+                    '<td onclick="deleteVor('+x.von+', '+x.bis+','+x.Kennzeichen+');" >Stornieren</td> \n'+
                     '</tr> \n' ;
                      }
             }
