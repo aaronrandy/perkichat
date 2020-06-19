88e8705459af87917eecb3a7fbbc5b75bba34547
Max <maximilian.mosch@smail.th-koeln.de>
2020-06-19 19:44:01 +0200

    logout angepasst

--- public/src/loginstartseite.js
+++ public/src/loginstartseite.js
@@ -95,12 +95,14 @@ function getCookie() {
     return whole_cookie;
 }
 
-function logout( name, path, domain ) {
-    if( get_cookie( name ) ) {
-        document.cookie = name + "=" +
-            ((path) ? ";path="+path:"")+
-            ((domain)?";domain="+domain:"") +
-            ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
+function logout() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1973 00:00:00 GMT";
     }
 }
 
