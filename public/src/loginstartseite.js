0008d4c1f259c930cc6b679041cf27f0f3556681
aomari99 <aomari99@users.noreply.github.com>
2020-06-19 18:10:03 +0200

    Update loginstartseite.js

--- public/src/loginstartseite.js
+++ public/src/loginstartseite.js
@@ -66,7 +66,7 @@ function deleteVor(a, b, c) {
     console.log(a + " " + b + " " + c +" ");
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4/* && xhttp.status == 200 */) {
+        if (xhttp.readyState == 4 && xhttp.status == 200 ) {
             var data = JSON.parse(xhttp.responseText);
             console.log(data.Status);
             if(data.Status == true){
