f6b41aece2ba109fb8034d61239e04d319ce4139
aomari99 <aomari99@users.noreply.github.com>
2020-06-19 17:06:51 +0200

    Update loginstartseite.js

--- public/src/loginstartseite.js
+++ public/src/loginstartseite.js
@@ -43,7 +43,7 @@ function loadVorbestellung() {
                     ' <td>'+x.bis+'</td> \n'+
                     ' <td>'+x.Kennzeichen+'</td> \n'+
                     ' <td>'+x.Bemerkung+'</td> \n'+
-                        '<td><button type="button" class="btn btn-primary">Bearbeiten</button><button type="button" onclick="deleteVorbestellung('+x.von+", "+x.bis+", "+x.Kennzeichen+')" class="btn btn-danger">Stornieren</button></td>'+
+                        '<td><button type="button" class="btn btn-primary">Bearbeiten</button><button type="button" onclick="deleteVor('+x.von+", "+x.bis+", "+x.Kennzeichen+')" class="btn btn-danger">Stornieren</button></td>'+
                     '</tr> \n' ;
                      }
             }
@@ -60,12 +60,41 @@ function loadVorbestellung() {
 }
 
 
+
+function deleteVor(a,b,c) {
+ 
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200 ) {
+            var data = JSON.parse(xhttp.responseText);
+            console.log(data.Status);
+            if(data.Status == true){
+                console.log(data);
+            }
+            else if(xhttp.readyState == 4 && xhttp.status != 200)
+                 window.location.replace("https://parkouni.tk/404");
+        }
+    };
+
+    
+    xhttp.open("DELETE", "https://parkouni.tk/api/Vorbestellung?apikey=101", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("von="+a+"&bis="+b+"&kenn="+c);
+
+}
+
+
+
+
 function getCookie() {
     var nameEquals ="logindaten=";
     var whole_cookie=document.cookie.split(nameEquals)[1].split(";")[0];   
     console.log(whole_cookie);
     return whole_cookie;
 }
+
+
+
 
 function logout() {
     document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
