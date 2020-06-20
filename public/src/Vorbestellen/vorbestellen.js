301777c004cb62dc45f58ad467cdc428924ed850
aomari99 <aomari99@users.noreply.github.com>
2020-06-20 03:49:25 +0200

    Update vorbestellen.js

--- public/src/Vorbestellen/vorbestellen.js
+++ public/src/Vorbestellen/vorbestellen.js
@@ -122,7 +122,7 @@ function getPK(x) {
                 console.log(Kunde);
                 console.log("AFFE::: " +carselections);
                     for(let x of Kunde) 
-                        carselections.append("\n<option value=\""+x.Pnr+"\">"+x.Pnr+"</option> ");
+                        carselections.appendChild("\n<option value=\""+x.Pnr+"\">"+x.Pnr+"</option> ");
                     
 
             }
